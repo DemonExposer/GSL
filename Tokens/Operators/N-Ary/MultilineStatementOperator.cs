b42284988009ec1fefc2741d8968dac7461d0cd4
DemonExposer <kbdmns@gmail.com>
2022-07-19 16:34:22 +0200

    Fixed nested return statements

--- Tokens/Operators/N-Ary/MultilineStatementOperator.cs
+++ Tokens/Operators/N-Ary/MultilineStatementOperator.cs
@@ -1,3 +1,4 @@
+using Interpreter.Tokens.Statements.Binary;
 using Interpreter.Tokens.Statements.Unary;
 using TrieDictionary;
 using Object = Interpreter.Types.Object;
@@ -11,8 +12,14 @@ public class MultilineStatementOperator : NAryOperator {
 
 	public override Object Evaluate(List<TrieDictionary<Object>> vars) {
 		foreach (Token t in Children) {
-			if (t is ReturnStatement)
+			if (t is ReturnStatement) // TODO: add flag to check if this is part of a function
 				return t.Evaluate(vars);
+
+			if (t is BinaryStatement) {
+				Object obj = t.Evaluate(vars);
+				if (obj != null!)
+					return obj;
+			}
 			
 			t.Evaluate(vars);
 		}
