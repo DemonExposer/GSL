6c404728f53b701310baf19d2fd04bd7200958ba
DemonExposer <kbdmns@gmail.com>
2022-07-24 19:05:44 +0200

    Basic implementation of properties and methods for predefined data types finished

--- Tokens/Operators/Binary/DotOperator.cs
+++ Tokens/Operators/Binary/DotOperator.cs
@@ -1,4 +1,8 @@
+using Interpreter.Types.Comparable;
+using Interpreter.Types.Function;
+using Interpreter.Types.Util;
 using TrieDictionary;
+using Array = Interpreter.Types.Array;
 using Object = Interpreter.Types.Object;
 
 namespace Interpreter.Tokens.Operators.Binary; 
@@ -8,5 +12,25 @@ public class DotOperator : BinaryOperator {
 		Symbol = ".";
 	}
 
-	public override Object Evaluate(List<TrieDictionary<Object>> vars) => Left.Evaluate(vars).Properties[Right.Str];
+	public override Object Evaluate(List<TrieDictionary<Object>> vars) {
+		 Object res = Left.Evaluate(vars).Properties[Right.Str];
+
+		 VariableToken properRight = (VariableToken) Right;
+		 if (res is Function f) {
+			 Object o = properRight.Args.Evaluate(vars);
+			 return f.Execute(o is ArgumentArray aa ? aa.Arr : new [] {o}, vars);
+		 }
+
+		 if (res is Array a && properRight.Index != null!) {
+			 Array arr = (Array) properRight.Index.Evaluate(vars);
+			 if (arr.Arr.Count != 1 || arr.Arr[0] is not Integer)
+				 throw new FormatException("Line " + Line +  ": index must be of type Integer");
+
+			 Integer i = (Integer) arr.Arr[0];
+			 Index index = i.Int >= 0 ? new Index(i.Int) : ^-i.Int; // Negative index will take nth element from the right
+			 return a.Arr[index];
+		 }
+
+		 return res;
+	}
 }
--- Types/LowObject.cs
+++ Types/LowObject.cs
@@ -5,7 +5,7 @@ namespace Interpreter.Types;
 public class LowObject {
 	public Class ClassType = null!;
 	public TrieDictionary<Object> Properties = new TrieDictionary<Object>();
-	
+
 	public new virtual string ToString() => "Object";
 
 	public new virtual string GetType() => "Object";
