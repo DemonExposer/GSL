c0eaffc02dd66e03134c3b22b2bc67c51cc195f3
DemonExposer <kbdmns@gmail.com>
2022-08-18 18:56:05 +0200

    Fixed printing of nested dictionaries

--- Types/Dictionary.cs
+++ Types/Dictionary.cs
@@ -5,11 +5,18 @@ namespace Interpreter.Types;
 public class Dictionary : Object {
 	public override string GetType() => "Dictionary";
 
-	public override string ToString() { // TODO: Make this indent nested dictionaries correctly
-		StringBuilder sb = new StringBuilder("{\n");
+	public override string ToString() => ToString(0);
+	
+	public string ToString(int indent) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < indent; i++)
+			sb.Append('\t');
+		string indentStr = sb.ToString();
+		
+		sb = new StringBuilder("{\n");
 		foreach (string s in Properties.GetKeySet())
-			sb.Append($"\t{s}: {Properties[s].ToString()}\n");
-		sb.Append('}');
+			sb.Append(indentStr).Append($"\t{s}: {(Properties[s] is Dictionary dict ? dict.ToString(indent + 1) : Properties[s].ToString())}\n");
+		sb.Append(indentStr).Append('}');
 
 		return sb.ToString();
 	}
